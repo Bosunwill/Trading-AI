dce61f9838f5dc9eead1ab6bfc1c2eb85a2e0012
wbelden <60723838+wbelden@users.noreply.github.com>
2022-04-05 14:57:35 -0500

    Kivy GUI 0.1.1
    Created more test files for kivy GUI

--- User Interface/GUI-main.py
+++ User Interface/GUI-main.py
@@ -1,45 +1,19 @@
 import kivy
+from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.config import Config
 
 kivy.require('1.10.0')
 
-from kivy.app import App
-from kivy.uix.button import Label
-from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, Color
-
-class CanvasWidget(Widget):
-    def __init__(self, **kwargs):
-        super(CanvasWidget, self).__init__(**kwargs)
-        with self.canvas:
-            Color(.234, .456, .678, .8)
-            self.rect = Rectangle(pos = self.center, size = (self.width / 2., self.height / 2.))
-            self.label = Label(text = "Trading AI App", halign = "center", valign = "center")
-
-            self.bind(pos = self.update_rect, size = self.update_rect)
-
-    def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.label.pos = self.pos
-        self.rect.size = self.size
-
-# class LabelWidget(GridLayout):
-#     bones = 0
-#     def __init__(self, **kwargs):
-#         super(LabelWidget, self).__init__(**kwargs)
-#         self.label_text = Label(text = "Trading AI App")
-#         self.add_widget(self.label_text)
-#         def update_text(self, text):
-#             LabelWidget.bones = text
-#             self.label_text.text = str(LabelWidget().bones)
-#             LabelWidget.text = str(LabelWidget().bones)
+Config.set('graphics', 'resizable', True)
 
 class TradingAI(App):
     def build(self):
-        canvas = CanvasWidget()
-        return canvas
+        Fl = FloatLayout()
+        btn = Button(text = 'Hello world', size_hint = (.3, .5), background_color = (.1, .4, .5), pos_hint = {'x':.2, 'y':.2})
+        Fl.add_widget(btn)
+        return Fl
 
-
-tradingAI = TradingAI()
-tradingAI.run()
+if __name__ == '__main__':
+    TradingAI().run()
