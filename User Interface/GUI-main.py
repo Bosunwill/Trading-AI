770a03b4d85ea6f37449fe127d998ed986309dc7
wbelden <60723838+wbelden@users.noreply.github.com>
2022-05-01 00:19:55 -0500

    Completed layout for app

--- User Interface/GUI-main.py
+++ User Interface/GUI-main.py
@@ -1,19 +1,17 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.config import Config
+from kivy.uix.pagelayout import PageLayout
 
 kivy.require('1.10.0')
 
-Config.set('graphics', 'resizable', True)
+class MultipleLayout(PageLayout):
+    pass
 
 class TradingAI(App):
     def build(self):
-        Fl = FloatLayout()
-        btn = Button(text = 'Hello world', size_hint = (.3, .5), background_color = (.1, .4, .5), pos_hint = {'x':.2, 'y':.2})
-        Fl.add_widget(btn)
-        return Fl
+
+        return MultipleLayout()
+
 
 if __name__ == '__main__':
     TradingAI().run()
